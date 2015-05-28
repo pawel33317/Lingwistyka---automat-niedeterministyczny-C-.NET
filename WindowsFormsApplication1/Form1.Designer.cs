77ea793080da8cb2ac7fdbcc4b5f9bc2fa2a3619
Pawel Czubak <pawel33317@gmail.com>
2015-05-28 19:47:30 +0200

    Finalny automat
    Signed-off-by: Pawel Czubak <pawel33317@gmail.com>

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bLoadFile = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.bReadFile = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // bLoadFile
+            // 
+            this.bLoadFile.Location = new System.Drawing.Point(12, 12);
+            this.bLoadFile.Name = "bLoadFile";
+            this.bLoadFile.Size = new System.Drawing.Size(75, 23);
+            this.bLoadFile.TabIndex = 0;
+            this.bLoadFile.Text = "Załaduj plik";
+            this.bLoadFile.UseVisualStyleBackColor = true;
+            this.bLoadFile.Click += new System.EventHandler(this.bLoadFile_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(105, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Brak pliku";
+            // 
+            // bReadFile
+            // 
+            this.bReadFile.Location = new System.Drawing.Point(282, 13);
+            this.bReadFile.Name = "bReadFile";
+            this.bReadFile.Size = new System.Drawing.Size(75, 23);
+            this.bReadFile.TabIndex = 2;
+            this.bReadFile.Text = "Odczytaj zawartość pliku";
+            this.bReadFile.UseVisualStyleBackColor = true;
+            this.bReadFile.Click += new System.EventHandler(this.bReadFile_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(12, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Zawartość: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(377, 17);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Separator: ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(445, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "###";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(12, 181);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Przejścia: ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.Location = new System.Drawing.Point(12, 291);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Wynik: ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 314);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(43, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Wynik: ";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 203);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(55, 13);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Przejścia: ";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 71);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 13);
+            this.label8.TabIndex = 10;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(508, 176);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Uruchom";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1009, 439);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.bReadFile);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.bLoadFile);
+            this.Name = "Form1";
+            this.Text = "Automat niedeterministyczny";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bLoadFile;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button bReadFile;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        string[] tablicaZdan;
+        string input = "";
+
+        List<int> currentStates = new List<int>();
+
+        String []statesName = {"SP","S0","S1","S2","S3","S4","S5","S6","S7","S8","S9","SK","SZ"};
+        int[,] statesTable = 
+        {
+            {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, //SP 1-->0, 2-->1 ...
+            {11,12,12,12,12,12,12,12,12,12}, //0
+            {12,11,12,12,12,12,12,12,12,12}, //1
+            {12,12,11,12,12,12,12,12,12,12}, //2
+            {12,12,12,11,12,12,12,12,12,12}, //3
+            {12,12,12,12,11,12,12,12,12,12}, //4
+            {12,12,12,12,12,11,12,12,12,12}, //5
+            {12,12,12,12,12,12,11,12,12,12}, //6
+            {12,12,12,12,12,12,12,11,12,12}, //7
+            {12,12,12,12,12,12,12,12,11,12}, //8
+            {12,12,12,12,12,12,12,12,12,11}, //9
+            {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1}, //SK dobry koniec znalazlo powtórzenie
+            {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1}  //SZ zly brak powtórzenia 
+        };
+
+        private void bLoadFile_Click(object sender, EventArgs e)
+        {
+            label6.Text = "";
+            List<int> stringList = new List<int> { };
+
+            int size = -1;
+            DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+            if (result == DialogResult.OK) // Test result.
+            {
+                string file = openFileDialog1.FileName;
+                try
+                {
+                    input = File.ReadAllText(file);
+                    size = input.Length;
+                }
+                catch (IOException)
+                {
+                    MessageBox.Show("Error");
+                }
+                finally
+                {
+                    label1.Text = openFileDialog1.FileName.ToString();
+                }
+            }
+        }
+        private void bReadFile_Click(object sender, EventArgs e)
+        {
+            label8.Text = "";
+            tablicaZdan = input.Split(new string[] { textBox1.Text }, StringSplitOptions.None);
+            foreach (string s in tablicaZdan)
+            {
+                label8.Text += s + "\n";
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label7.Text = "";
+            label6.Text = "";
+            //dla każdego zdania
+            foreach (string s in tablicaZdan)
+            {
+                int position = 0;
+                bool znalezionoPowtorzenie = false;
+                currentStates.Clear();
+                currentStates.Add(0);
+                //dla każdego znaku
+                foreach (char c in s)
+                {
+                    
+                    int podanyZnak = (int)Char.GetNumericValue(c);
+
+
+                    //dla każdego stanu w którym aktualnie jest
+                    List<int> tmpcurrentStates = new List<int>(currentStates);
+                    currentStates.Clear();
+
+                    //z każdego starego stanu przechodzi do nowego
+                    foreach (int stan in tmpcurrentStates)
+                    {
+                        //wypisujemy stany w których jest aktualnie
+                        label7.Text += statesName[stan] + " ";
+
+                        //przechodzimy do nowych stanów
+                        if (statesTable[stan, podanyZnak] != -1)
+                            currentStates.Add(statesTable[stan, podanyZnak]);
+
+                        //sprawdzamy czy nie jest w stanie 
+                        if (stan == 11)
+                            znalezionoPowtorzenie = true;
+                    }
+                    currentStates.Add(0); if (znalezionoPowtorzenie) break;
+                    label7.Text += " --> ";
+                    label7.Text += " [" + c + "] ";
+                    position++;
+                }
+                if (znalezionoPowtorzenie)
+                {
+                    label7.Text += "ZNALEZIONO";
+                    label6.Text += s + "  -->  " + (position-1) + "\n";
+                }
+                else
+                {
+                    //dopisuje ostatnie stany na koniec w któych się znajduje
+                    foreach (int stan in currentStates)
+                    {
+                        label7.Text += statesName[stan] + " ";
+                    }
+                    label7.Text += "BRAK";
+                }
+                label7.Text += "\n";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
+
